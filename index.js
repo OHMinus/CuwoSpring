cfb45e883c1052e94269abba38a8c58724254be7
TACT21 <53495996+TACT21@users.noreply.github.com>
2025-02-07 02:51:28 +0900

    Add files

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+  liff
+    .init({ liffId: process.env.LIFF_ID })
+    .then(() => {
+        console.log("Success! you can do something with LIFF API here.")
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+});
